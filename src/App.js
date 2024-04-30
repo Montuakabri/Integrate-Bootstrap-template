8450810f2aea0b175d06d7030a0679d25d212623
montu akbari <akbarimontu@gmail.com>
2024-04-30 09:08:03 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import Navbar from './componet/Navbar';
+import Banner from './componet/Banner';
+import Footer from './componet/Footer';
+import Services from './componet/Services';
+import Tour from './componet/Tour';
+import Package from './componet/Package';
+import Contact from './componet/Contact';
+
+
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <Banner/>
+    <Services />
+    <Tour/>
+    <Package/>
+    <Contact/>
+    <Footer/>
+    </>
+  );
+}
+
+export default App;
